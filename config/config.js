10b7301cf6f0ccfea5e08588c98f29301c6b951a
Alejandro Gonzalez <alejandro@plasticolab.com>
2022-10-22 05:07:19 -0500

    ajustes para despliegue en Heroku

--- config/config.js
+++ config/config.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const config = {
   env: process.env.NODE_ENV || 'dev',
+  isProd: process.env.NODE_ENV === 'production',
   port: process.env.PORT || 3000,
   dbUser:  process.env.DB_USER,
   dbPassword:  process.env.DB_PASSWORD,
@@ -10,6 +11,7 @@ const config = {
   dbPort:  process.env.DB_PORT,
   apikey:  process.env.API_KEY,
   jwtSecret:  process.env.JWT_SECRET,
+  dbUrl: process.env.DATABASE_URL,
 }
 
 module.exports = { config };
