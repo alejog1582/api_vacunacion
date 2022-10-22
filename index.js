fd7585381c687a89c16b865b2970f25935db8bb6
Alejandro Gonzalez <alejandro@plasticolab.com>
2022-10-22 04:02:14 -0500

    inclusion cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors= require("cors");
 const routerApi = require("./routes");
 
 const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } =  require('./middlewares/error.handler')
@@ -7,6 +8,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
 
 require('./utils/auth')
 
